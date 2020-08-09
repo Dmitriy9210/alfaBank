2b7d2f118e169b75d623325d91f8a125dae5d963
kefir <lala@mail.ru>
2020-08-09 14:19:35 +0300

    Работа с методами sibling(), preceding(), parent(), closest()

--- src/test/java/TestAlfaBankInsurance.java
+++ src/test/java/TestAlfaBankInsurance.java
@@ -1,32 +1,49 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestAlfaBankInsurance {
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         open("https://alfabank.ru/make-money/savings-account/");
-        //переходим по Депозитам
     }
 
     @Test
-    public void workInsuranceSibling() {
+    void workInsuranceUseSibling() { //работа с sibling()
 
         $(".navigation").$(byText("Депозиты")).click();
-        //проверяем переключилась ли страница и появилось ли окно Депозиты
+        //проверяем переключилась ли страница и появилось ли окно Депозиты, а после берем 3 братский элемент от него
         $(".selected").shouldBe(text("Депозиты")).sibling(3).click();
         $(".frame-head").shouldBe(text("Страхование вкладов"));
     }
 
     @Test
-    public void workInsurancePreceding() {
+    void workInsuranceUsePreceding() { //работа с Preceding()
 
         $(".product-cell").preceding(0).$(byText("Страхование вкладов")).click();
+        $(".frame-head").shouldBe(text("Страхование вкладов"));
+    }
+
+    @Test
+    void workInsuranceUseParent() { // работа c Parent()
+        $$(byTitle("Инвестиции")).get(2).parent().sibling(0).click(); //селекторы немного кривые, но моя задача проверить работу метода parent()
+        $(".frame-head").shouldBe(text("Страхование вкладов"));
+
+    }
+
+    @Test
+    void workInsuranceUseClosest() { //работа c Closest()
+        $$(byTitle("Инвестиции"))
+                .get(2)
+                .parent()
+                .closest("ul")
+                .$(byText("Страхование вкладов"))
+                .click(); //селекторы немного кривые
         $(".frame-head").shouldBe(text("Страхование вкладов"));
     }
 }
